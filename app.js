50003b9ce788173f295309b5e28f7968a5ce1b3a
ahp2604 <andy.tan.ho@gmail.com>
2018-05-19 11:44:42 -0500

    fixed up user,postions.js

--- app.js
+++ app.js
@@ -21,11 +21,15 @@ let db = require('./models');
 db.sequelize.sync({force: true}).then(()=>{
     app.listen(process.env.PORT | 3000, ()=>{
         console.log('Time Clock Server Running...');
+        db.Position.create({
+            position_title: "manager"
+        })
         db.User.create({
             first_name: "John",
             last_name: "Lagmay",
             email: "jj@jj.com",
-            password: "password"
+            password: "password",
+            PositionId: "1"
         })
     })
 })
