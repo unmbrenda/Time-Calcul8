eb71361250991148dffec5f6bcf40f84ada12eb2
John <jjohnp5@gmail.com>
2018-05-22 19:00:27 -0500

    updated prod to push heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let bodyParser = require('body-parser');
 let passport = require('passport');
 let LocalStrategy = require('passport-local').Strategy;
 let session = require("express-session")
-let flash = require('connect-flash'); 
+let flash = require('connect-flash');
 
 let db = require('./models');
 let bcrypt = require('bcrypt');
@@ -14,67 +14,67 @@ app.set("views", path.join(__dirname, '/views'));
 app.engine("handlebars", exphbs({ defaultLayout: "index" }));
 app.set('view engine', 'handlebars')
 
-passport.use('employee', new LocalStrategy((username, password, done)=>{
+passport.use('employee', new LocalStrategy((username, password, done) => {
     db.User.findOne({
         where: {
             id: username
         }
-    }).then(data=>{
+    }).then(data => {
         let pw = data.getDataValue('password');
-            bcrypt.compare(password, pw, (err, response)=>{
-                if(err){
-                    return done(null, false, {message: 'Incorrect username or password'});
-                }
-                return done(null, data);
-            })
+        bcrypt.compare(password, pw, (err, response) => {
+            if (err) {
+                return done(null, false, { message: 'Incorrect username or password' });
+            }
+            return done(null, data);
+        })
     })
 }))
-passport.use('manager', new LocalStrategy((username, password, done)=>{
+passport.use('manager', new LocalStrategy((username, password, done) => {
     db.User.findOne({
         where: {
             id: username
         }
-    }).then(data=>{
+    }).then(data => {
         let pw = data.getDataValue('password');
-        if(data.PositionId !== 2){
-            return done(null, false, {message: 'You are not a manager'});
-        }else{
-            bcrypt.compare(password, pw, (err, response)=>{
-                if(err){
-                    return done(null, false, {message: 'Incorrect username or password'});
+        if (data.PositionId !== 2) {
+            return done(null, false, { message: 'You are not a manager' });
+        } else {
+            bcrypt.compare(password, pw, (err, response) => {
+                if (err) {
+                    return done(null, false, { message: 'Incorrect username or password' });
                 }
                 return done(null, data);
             })
         }
-            
+
     })
 }))
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user.id);
-  });
-  
-  passport.deserializeUser(function(id, done) {
-    db.User.findById(id).then(user =>{
-       
-      done(null, user);
+});
+
+passport.deserializeUser(function (id, done) {
+    db.User.findById(id).then(user => {
+
+        done(null, user);
     });
-  });
+});
 app.use(express.static("public"));
-app.use(session({ secret: "money", resave: false, saveUninitialized: false}));
+app.use(session({ secret: "money", resave: false, saveUninitialized: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash())
 
-app.use(function(req,res, next){
+app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
     res.locals.error = req.flash('error');
     res.locals.success = req.flash('success')
     next();
- });
+});
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render('index')
 })
 let loginRoute = require('./routes/login');
@@ -98,29 +98,29 @@ app.use('/', htmlRouter);
 
 
 
-db.sequelize.sync().then(()=>{
-    app.listen(process.env.PORT || 3000, ()=>{
+db.sequelize.sync({force: true}).then(() => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log('Time Clock Server Running...');
-        // db.Position.create({
-        //     position_title: "employee",
-        //     id: 1
-        // })
-        // db.Position.create({
-        //     position_title: "manager",
-        //     id: 2
-        // })
-        // bcrypt.hash('timeismoney', 10, (err, hash)=>{
-        //     db.User.create({
-        //         first_name: 'Super',
-        //         last_name: 'User',
-        //         email: "superuser@eztime.db",
-        //         password: hash,
-        //         PositionId: 2
-        //     }).then(data=>{
-        //         console.log('create success')
-        //     }).catch(err=>{
-        //         console.log('failed create')
-        //     })
-        // })
+        db.Position.create({
+            position_title: "employee",
+            id: 1
+        })
+        db.Position.create({
+            position_title: "manager",
+            id: 2
+        })
+        bcrypt.hash('timeismoney', 10, (err, hash)=>{
+            db.User.create({
+                first_name: 'Super',
+                last_name: 'User',
+                email: "superuser@eztime.db",
+                password: hash,
+                PositionId: 2
+            }).then(data=>{
+                console.log('create success')
+            }).catch(err=>{
+                console.log('failed create')
+            })
+        })
     })
 })
